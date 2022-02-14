75e7d3ae50528c72feb679503d5087ad87b1b69b
rafajlpz <frilo_94@hotmail.com>
2022-02-14 12:55:42 +0100

    Validar formulario Vue.js
    -Validar el arroba
    -Minimo de caracteres en el password
    -Deshabilitar boton registar.

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/editar/:id',
+    name: 'Editar',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Editar.vue')
+  },
+  {
+    path: '/registro',
+    name: 'Registro',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Registro.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Registro.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1 class="my-5">Registro de usuarios</h1>
+  <form>
+    <!-- .trim se usa para limpiar espacios en blanco -->
+    <input
+      type="email"
+      placeholder="email"
+      class="form-control my-2"
+      v-model.trim="email"
+    />
+
+    <input
+      type="password"
+      placeholder="password"
+      class="form-control my-2"
+      v-model.trim="pass1"
+    />
+
+    <input
+      type="password"
+      placeholder="password"
+      class="form-control my-2"
+      v-model.trim="pass2"
+    />
+
+    <button type="submit" class="btn btn-primary" :disabled="bloquear">
+      Registrar
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  //Uso de vdata para capturar el email y el password
+  data() {
+    return {
+      email: "",
+      pass1: "",
+      pass2: "",
+    };
+  },
+  computed: {
+    bloquear() {
+      if (!this.email.includes("@")) {
+        return true
+      }
+      if (this.pass1.length > 5 && this.pass1 === this.pass2) {
+        return false
+      }
+      return true
+    }
+  }
+};
+</script>
